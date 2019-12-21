5014a892ec10982670f67abc2a4f291372d6676b
Abhi Sinha <sinha45@purdue.edu>
2019-12-21 11:18:00 -0500

    setup package

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-
+import os
 import sys
 
 from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 # Think about this later... I'm lazy right now
 #   Supporting earlier versions of Python 3 may be simple enough, 
@@ -10,7 +12,9 @@ from setuptools import setup
 if sys.version[:2] < (3, 7):
     raise RuntimeError("EliasFano only supports Python 3.7 or later")
 
-# 
+package_info = dict()
+with open(os.path.join(here, "eliasfano", "__about__.py"), 'r') as a:
+    exec(a.read(), package_info)
 
 
 if __name__ == "__main__":
